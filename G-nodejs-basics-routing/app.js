b70faee785d21df65cac8cc41d4aa97e0acb81d5
Naveen-Gupta <nvngupta1992@gmail.com>
2017-06-10 23:44:21 +0530

    Delete app.js

--- G-nodejs-basics-routing/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = 8000;
-const server = require('./server');
-
-http.createServer(server.handleRequest)
-    .listen(port, () => {
-        console.log("server is running now..");
-    });//listen
